cd22108e5a044c1de1d985b1122617ea9e207e99
Jivesh <ljivesh4@gmail.com>
2020-07-17 10:23:47 +0530

    Main Menu Format

---
+++ T_with_t.py
@@ -0,0 +1,35 @@
+from tkinter import *
+from PIL import Image, ImageTk
+
+
+
+if __name__ == '__main__':
+
+    main_menu = Tk()    #main menu window
+    menu_canvas = Canvas(main_menu, height = 580, width = 600)  #Canvas
+    menu_canvas.pack()
+
+    #Main Menu Background
+    bg_img = ImageTk.PhotoImage(Image.open("Images\MainMenu.jpg"))  #Making .jpg format compatible with tkinter
+    bg_img_label = Label(menu_canvas, image = bg_img)               #Using background image as label
+    bg_img_label.place(relheight = 1, relwidth = 1)                 #placeing background image
+
+
+
+    #Main Menu Buttons
+
+    #Play Button
+    play_button = Button(menu_canvas, text = 'Play', bg = '#E3CBB9', relief = 'groove', padx = 1, pady = 1)
+    play_button.place(relx = 0.7, rely = 0.5, relheight = 0.08, relwidth = 0.25)
+
+    #Options Button
+    options_button = Button(menu_canvas, text = 'Options', bg = '#E3CBB9', relief = 'groove', padx = 1, pady = 1)
+    options_button.place(relx = 0.7, rely = 0.6, relheight = 0.08, relwidth = 0.25)
+
+    #Exit Button
+    exit_button = Button(menu_canvas, text = 'Exit', bg = '#E3CBB9', relief = 'groove', padx = 1, pady = 1)
+    exit_button.place(relx = 0.7, rely = 0.7, relheight = 0.08, relwidth = 0.25)
+
+
+
+    main_menu.mainloop()
